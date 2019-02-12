672248e8689d136d0d0dee3ac4a1c8cf08394f68
mega-hertz91 <mega-hertz@bk.ru>
2019-02-12 17:52:51 +0400

    add queries.sql

---
+++ queries.sql
@@ -0,0 +1,14 @@
+INSERT INTO users (niname, email, password)
+ VALUES ('Ricentwo', 'Ricentwo-4181@yopmail.com', 'Ricentwo-4181'),
+ VALUES ('Ocollojir', 'ocollojir-0780@yopmail.com', 'Ocollojir-0780'),
+ VALUES ('Qurrellizaha-0662', 'Qurrellizaha-0662@yopmail.com', 'Qurrellizaha-0662'),
+ VALUES ('Amullowa', 'Amullowa-6503@yopmail.com', 'Amullowa-6503'),
+ VALUES ('Denubipac', 'denubipac-6324@yopmail.com', 'denubipac-632');
+
+INSERT INTO category (categiory)
+ VALUE ('Доски и лыжи'),
+  ('Крепления'),
+  ('Ботинки'),
+  ('Одежда'),
+  ('Инструменты'),
+  ('Разное');
