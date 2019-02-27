2b948f3ed364c0974bc8e1eb571dc962315498b1
mega-hertz91 <mega-hertz@bk.ru>
2019-02-27 18:41:06 +0400

    Добавляет форму входа

--- enter.php
+++ enter.php
@@ -3,6 +3,11 @@ require_once ('inc/config.php');
 require_once ('inc/sql.php');
 require_once ('inc/function.php');
 
-$layout_content = include_template('layout_lot.php', ['content' => '','categories' => render_categories($con), 'lot' => ['name' => 'Вход на сайт']]);
+$form_data = [];
+$errors = [];
+
+$page_content = include_template('_enter.php', ['categories' => render_categories($con), 'form_data' => $form_data, 'errors' => $errors]);
+
+$layout_content = include_template('layout_lot.php', ['content' => $page_content,'categories' => render_categories($con), 'lot' => ['name' => 'Вход на сайт']]);
 
 print ($layout_content);
