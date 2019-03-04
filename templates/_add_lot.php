8dec7c22fe6b1174b5f88bd01621f41a43e0bef5
mega-hertz91 <mega-hertz@bk.ru>
2019-03-04 19:33:11 +0400

    Подготовка к допуску

--- templates/_add_lot.php
+++ templates/_add_lot.php
@@ -3,7 +3,7 @@
             <div class="form__container-two">
                 <div class="form__item <?php if(check_input($errors, 'lot-name') == true): ?>form__item--invalid<?php endif;?>"> <!-- form__item--invalid -->
                     <label for="lot-name">Наименование</label>
-                    <input id="lot-name" type="text" name="lot-name" value="<?=$form_data['lot-name']?>" placeholder="Введите наименование лота" >
+                    <input id="lot-name" type="text" name="lot-name" value="<?=$form_data['lot-name']?>" placeholder="Введите наименование лота" required>
                     <span class="form__error">Введите наименование лота</span>
                 </div>
                 <div class="form__item">
@@ -21,7 +21,7 @@
             </div>
             <div class="form__item form__item--wide <?php if(check_input($errors, 'lot-message') == true): ?>form__item--invalid<?php endif;?>">
                 <label for="message">Описание</label>
-                <textarea id="message" name="message" placeholder="Напишите описание лота" ><?=$form_data['message']?></textarea>
+                <textarea id="message" name="message" placeholder="Напишите описание лота"required ><?=$form_data['message']?></textarea>
                 <span class="form__error">Напишите описание лота</span>
             </div>
             <div class="form__item form__item--file <?php if(check_input($errors, 'image-lot') == true): ?>form__item--invalid<?php endif;?>"> <!-- form__item--uploaded -->
@@ -33,7 +33,7 @@
                     </div>
                 </div>
                 <div class="form__input-file">
-                    <input class="visually-hidden" type="file" id="photo2" name="image-lot" value="" >
+                    <input class="visually-hidden" type="file" id="photo2" name="image-lot" value="" required>
                     <label for="photo2">
                         <span>+ Добавить</span>
                     </label>
@@ -43,12 +43,12 @@
             <div class="form__container-three">
                 <div class="form__item form__item--small <?php if(check_input($errors, 'lot-rate') == true): ?>form__item--invalid<?php endif;?>">
                     <label for="lot-rate">Начальная цена</label>
-                    <input id="lot-rate" type="number" name="lot-rate" placeholder="0" value="<?=$form_data['lot-rate']?>" >
+                    <input id="lot-rate" type="number" name="lot-rate" placeholder="0" value="<?=$form_data['lot-rate']?>" required>
                     <span class="form__error">Введите начальную цену</span>
                 </div>
                 <div class="form__item form__item--small <?php if(check_input($errors, 'lot-step') == true): ?>form__item--invalid<?php endif;?>">
                     <label for="lot-step">Шаг ставки</label>
-                    <input id="lot-step" type="number" name="lot-step" value="<?=$form_data['lot-step']?>" placeholder="0" >
+                    <input id="lot-step" type="number" name="lot-step" value="<?=$form_data['lot-step']?>" placeholder="0" required>
                     <span class="form__error">Введите шаг ставки</span>
                 </div>
                 <div class="form__item">
